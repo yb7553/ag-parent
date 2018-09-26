94c3d5778ceb048c1df64c51111126c433675940
yb7553 <23218371@qq.com>
2018-09-27 07:04:28 +0800

    脚手架

--- ag-common/src/main/java/com/github/yanbin/common/constants/CommonConstants.java
+++ ag-common/src/main/java/com/github/yanbin/common/constants/CommonConstants.java
@@ -3,6 +3,10 @@ package com.github.yanbin.common.constants;
 public class CommonConstants {
     public static final Integer EX_TOKEN_ERROR_CODE = 40101;
     public static final Integer EX_OTHER_CODE=500;
+
+    public static final String CONTEXT_KEY_USER_ID = "currentUserId";
+    public static final String CONTEXT_KEY_USERNAME = "currentUserName";
+    public static final String CONTEXT_KEY_USER_NAME = "currentUser";
     
 
 }
